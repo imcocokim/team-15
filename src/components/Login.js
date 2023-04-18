d0a775041ba3be520c978c437df7e3a9a63e8cc5
imcoco <imcoco@gmail.com>
2023-04-18 11:09:27 -0400

    set up login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,49 @@ import {
 
 const Login = () => {
     return (
-        <form>
+        <form 
+            autoComplete='off'
+            className='signupContainer'
+        >
+            <div>
+                <div className='label'>
+                    <label htmlFor="email">Email</label>
+                </div>
+                <div className='input'>
+                    <input 
+                        type="text" 
+                        autoComplete='off'
+                        id='email'
+                        name='email'
+                        placeholder='name@example.com'
+                        // value={email}
+                        // onChange={handChange}
+                    />
+                </div>
+            </div>
+            <div>
+                <div className='label'>
+                    <label htmlFor="password">Password</label>
+                </div>
+                <div className='input'>
+                    <input
+                        type='password'
+                        autoComplete='off'
+                        id='password'
+                        name='password'
+                        placeholder='****************'
+                        // value={password}
+                        //onChange={handleChange}
+                    />
+                </div>
+            </div>
+            <div>
+                <button>Log In</button>
+            </div>
+            <div>
+                <text>Don't have an account? <Link to='/signup'>Create free account</Link></text>
+            </div>
+
         </form>
     )
 }
